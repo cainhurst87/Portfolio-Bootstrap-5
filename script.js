38b2f8f24ff9a7563b1c5401d2c5eee5c0ddb3a9
Luiz Leal <143563882+cainhurst87@users.noreply.github.com>
2024-11-19 01:19:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleModeButton = document.getElementById('toggle-light-btn');
+    const htmlElement = document.documentElement;
 
+    const savedMode = localStorage.getItem('mode');
+    if (savedMode) {
+        htmlElement.setAttribute('data-mode', savedMode);
+        updateIcon(savedMode);
+    }
 
+    function toggleMode() {
+        const currentMode = htmlElement.getAttribute('data-mode');
+        const newMode = currentMode === 'dark' ? 'light' : 'dark';
+        htmlElement.setAttribute('data-mode', newMode);
+        localStorage.setItem('mode', newMode);
+        updateIcon(newMode);
+    }
 
+    function updateIcon(mode) {
+        const icon = toggleModeButton.querySelector('i');
+        if (mode === 'dark') {
+            icon.classList.remove('bi-moon');
+            icon.classList.add('bi-sun');
+        } else {
+            icon.classList.remove('bi-sun');
+            icon.classList.add('bi-moon');
+        }
+    }
 
-// TODO changeLanguage()
-// TODO toggleLight()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    toggleModeButton.addEventListener('click', toggleMode);
+});
